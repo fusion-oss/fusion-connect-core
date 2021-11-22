ab000661000843bb84e7e126bda12fb999f44c55
RakshantScopeRetail <rakshant.mishra@scoperetail.com>
2021-11-22 15:08:23 +0530

    space commit

--- src/main/java/com/scoperetail/fusion/connect/core/FusionConnectCoreConfig.java
+++ src/main/java/com/scoperetail/fusion/connect/core/FusionConnectCoreConfig.java
@@ -34,7 +34,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@Configuration
+@Configuration 
 @Import({AdapterConfig.class})
 @ComponentScan
 @EnableAutoConfiguration(
