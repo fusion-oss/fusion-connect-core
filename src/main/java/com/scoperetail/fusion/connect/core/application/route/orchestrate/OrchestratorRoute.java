048b47585d48c0f0428e0d527f54372f0fb6efcc
Javier Rivera Casanova <javier.casanova@scoperetail.com>
2021-12-27 10:47:34 -0600

    Log payload

--- src/main/java/com/scoperetail/fusion/connect/core/application/route/orchestrate/OrchestratorRoute.java
+++ src/main/java/com/scoperetail/fusion/connect/core/application/route/orchestrate/OrchestratorRoute.java
@@ -29,7 +29,6 @@ package com.scoperetail.fusion.connect.core.application.route.orchestrate;
 import java.util.List;
 import javax.annotation.PostConstruct;
 
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
