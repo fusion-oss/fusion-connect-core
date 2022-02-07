1dfec5e3e6b7b5611d35f539054a00ad11c4212f
ravi-zadpide <ravi.zadpide@scoperetail.com>
2022-02-07 18:15:54 +0530

    Implementation to remove target headers.

--- src/main/java/com/scoperetail/fusion/connect/core/application/route/orchestrate/OrchestratorRoute.java
+++ src/main/java/com/scoperetail/fusion/connect/core/application/route/orchestrate/OrchestratorRoute.java
@@ -12,10 +12,10 @@ package com.scoperetail.fusion.connect.core.application.route.orchestrate;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -102,10 +102,10 @@ public class OrchestratorRoute {
           .loop(exchangeProperty(ACTION_COUNT))
           .toD("${exchangeProperty.action_" + "${exchangeProperty.CamelLoopIndex}" + "}")
           .end()
+          .bean(SourceHeaderRemover.class)
           .choice()
           .when(exchangeProperty(IS_VALID_MESSAGE))
           .bean(DelimiterConfig.class)
-          .bean(SourceHeaderRemover.class)
           .recipientList(
               simple("${exchangeProperty.targetUri}"),
               simple("${exchangeProperty.targetDelimiter}").toString())
--- src/main/java/com/scoperetail/fusion/connect/core/application/route/orchestrate/bean/SourceHeaderRemover.java
+++ src/main/java/com/scoperetail/fusion/connect/core/application/route/orchestrate/bean/SourceHeaderRemover.java
@@ -12,10 +12,10 @@ package com.scoperetail.fusion.connect.core.application.route.orchestrate.bean;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,37 +27,26 @@ package com.scoperetail.fusion.connect.core.application.route.orchestrate.bean;
  */
 
 import java.util.Arrays;
-import java.util.Map;
-import java.util.Optional;
 import org.apache.camel.Exchange;
-import com.scoperetail.fusion.connect.core.config.Event;
+import org.apache.commons.lang3.StringUtils;
+import com.scoperetail.fusion.connect.core.common.constant.ExchangePropertyConstants;
+import com.scoperetail.fusion.connect.core.config.Source;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class SourceHeaderRemover {
 
-  private static final String REMOVE_SOURCE_HEADERS = "removeSourceHeaders";
-
   public void removeSourceHeaders(final Exchange exchange) {
-    final boolean isValidMessage = exchange.getProperty("isValidMessage", Boolean.class);
-    if (isValidMessage) {
-      final Optional<Object> optSourceHeaders = getHeaders(exchange);
-      if (optSourceHeaders.isPresent()) {
-        final String headersStr = String.valueOf(optSourceHeaders.get());
-        Arrays.stream(headersStr.split(","))
-            .forEach(
-                s -> {
-                  exchange.getIn().removeHeader(s);
-                  exchange.getIn().removeHeaders(s);
-                });
-        log.debug("Removed source headers: {}", headersStr);
-      }
+    final Source source = (Source) exchange.getProperty(ExchangePropertyConstants.SOURCE);
+    final String removeTargetHeadersStr = source.getRemoveTargetHeaders();
+    if (StringUtils.isNotBlank(removeTargetHeadersStr)) {
+      Arrays.stream(removeTargetHeadersStr.split(","))
+          .forEach(
+              s -> {
+                exchange.getIn().removeHeader(s);
+                exchange.getIn().removeHeaders(s);
+              });
+      log.debug("Removed target headers: {}", removeTargetHeadersStr);
     }
-  }
-
-  private Optional<Object> getHeaders(final Exchange exchange) {
-    final Event eventConfig = exchange.getProperty("event", Event.class);
-    final Map<String, Object> eventHeaders = eventConfig.getHeaders();
-    return Optional.ofNullable(eventHeaders.get(REMOVE_SOURCE_HEADERS));
   }
 }
